fc3ab1cc74e8b2a894f6d82a40ae4ba5a9e7bf72
frameshft <adiletkadyrov@gmail.com>
2017-07-05 16:42:54 +0600

    Add Chart component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map( weather => weather.main.temp);
-    
+
     return (
       <tr key={ name }>
         <td>{ name }</td>
+        <td>
+          <Chart data={temps} color='orange' />
+        </td>
       </tr>
     );
   }
 
   render() {
     return (
-      <table classNae='table table-hover'>
+      <table className='table table-hover'>
         <thead>
           <tr>
             <th>City</th>
